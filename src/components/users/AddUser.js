5b7cdb28e09a49d28f57d65873d438ba5a93324c
Sie7he <diegoescurraromero@gmail.com>
2022-01-05 18:36:29 -0300

    addUser v1.0.2

--- src/components/users/AddUser.js
+++ src/components/users/AddUser.js
@@ -8,102 +8,96 @@ export const AddUser = () => {
     const [nombres, setNombre] = useState('');
     const [apellidos, setApellido] = useState('');
     const [email, setEmail] = useState('');
-    const [perfil, setValue] = useState('');
+    const [value, setValue] = useState([]);
+    const [perfil, setPerfil] = useState('');
     const [telefono, setTelefono] = useState('');
-    const [selectedValue, setSelectedValue] = useState(null);
     const navigate = useNavigate();
 
-    const handleInputChange = value =>{
-        setValue(value || null)
-    }
-
-    const handleChange = value => {
-        setSelectedValue(value || null)
-    }
+ 
 
     const getPerfil = async () => {
         const url = process.env.REACT_APP_API + '/Usuario/Perfiles/get'
         const resp = await fetch(url);
         const data = await resp.json();
-        return data;
+        setValue(data);
     }
 
-    async function addUser() {
-        let hola = 1
-        let item = { nombres, apellidos, email, hola };
-        let result = await fetch(process.env.REACT_APP_API + "Administracion/Usuarios/save", {
-            method:"POST",
-            body:JSON.stringify(item)
-        });
+    getPerfil();
 
+    async function addUser() {
+        let perfil = 1
+        let item = { nombres, apellidos, perfil, email, telefono };
+        let result = await fetch(process.env.REACT_APP_API + "/Administracion/Usuarios/save", {
+            method: "POST",
+            body: JSON.stringify(item)
+        });
+        console.log(item)
         result = await result.json();
-        if(!result.correcto){
+        if (!result.correcto) {
             console.log(result.mensaje)
-        } else{
-           navigate('/user');
-        
+        } else {
+            navigate('/user');
+
         }
     }
 
     return (
         <div>
-         
-       <div className='col-md-4'>
-            <input
-                type="text"
-                name="nombres"
-                placeholder="Nombre"
-                onChange={(e) => setNombre(e.target.value)}
 
-            />
+            <select onChange={(e) => setPerfil(e.target.value)}>
+                    <option >Elegir perfil...</option>
+                {value.map((option) => (
+                    <option value={option.id}>{option.nombre}</option>
+                ))}
+            </select>
+
+
+
+            <div className='col-md-4'>
+                <input
+                    type="text"
+                    name="nombres"
+                    placeholder="Nombre"
+                    onChange={(e) => setNombre(e.target.value)}
+
+                />
             </div>
             <div className='col-md-4'>
-            <input
-                type="text"
-                name="apellidos"
-                placeholder="Apellido"
-                onChange={(e) => setApellido(e.target.value)}
+                <input
+                    type="text"
+                    name="apellidos"
+                    placeholder="Apellido"
+                    onChange={(e) => setApellido(e.target.value)}
 
-            />
+                />
             </div>
             <div className='col-md-4'>
-            <input
-                type="text"
-                name="telefono"
-                placeholder="telefono"
-                onChange={(e) => setTelefono(e.target.value)}
+                <input
+                    type="text"
+                    name="telefono"
+                    placeholder="telefono"
+                    onChange={(e) => setTelefono(e.target.value)}
 
-            />
+                />
             </div>
             <div className='col-md-4'>
-            <input
-                type="email"
-                name="email"
-                placeholder="Correo"
-                onChange={(e) => setEmail(e.target.value)}
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Correo"
+                    onChange={(e) => setEmail(e.target.value)}
 
-            />
+                />
             </div>
             <div className='col-md-4'>
-           <AsyncSelect 
-        cacheOptions
-        defaultOptions
-        value={selectedValue}
-        getOptionLabel={e => e.nombre}
-        getOptionValue={e => e.id}
-        loadOptions={getPerfil}
-        onInputChange={handleInputChange}
-        onChange={handleChange}
-       
-       
-       />
-       </div>
+            
+            </div>
             <div className='col-md-4'>
-            <button
-                onClick={addUser}
-            >
-                Agregar Usuario
-            </button>
+                <button
+                    onClick={addUser}
+                >
+                    Agregar Usuario
+                </button>
             </div>
         </div>
     )
